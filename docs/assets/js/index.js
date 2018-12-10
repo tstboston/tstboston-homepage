d0f2ee34848b9859928b48d894e389a1ae7f7a94
Matt Jardine <bordenjardine@gmail.com>
2018-12-09 22:29:10 -0500

    highlight current page

--- docs/assets/js/index.js
+++ docs/assets/js/index.js
@@ -1,16 +1,43 @@
 window.addEventListener('load', init);
+let sections = [];
 
 function init() {
+  sections = ['about',  'join', 'contact'].map(section => {
+    return {
+      section: document.querySelector(`#${section}`),
+      link: document.querySelector(`#${section}-link`)
+    }
+  });
   setupAutoScroll();
+  setupScrollDetect();
 }
 
 function setupAutoScroll() {
-  ['about', 'contact', 'join'].forEach(section => {
-    const link = document.querySelector(`#${section}-link`);
-		const dest = document.querySelector(`#${section}`);
-    link.addEventListener('click', event => {
+  sections.forEach(section => {
+    section.link.addEventListener('click', event => {
       event.preventDefault();
-      autoScroll(dest, 400, 'easeInQuad');
+      autoScroll(section.section, 400, 'easeInQuad');
     });
+  });
+}
+
+function setupScrollDetect() {
+  window.addEventListener('scroll', e => {
+    for(let i = 0; i < sections.length; i++) {
+      const scrollPos = window.scrollY;
+      const currentEl = sections[i].section;
+      const currentLink = sections[i].link;
+      const nextSection = sections[i + 1];
+      const nextEl = nextSection && nextSection.section;
+      console.log(scrollPos, currentEl.offsetTop);
+      if(
+        scrollPos >= currentEl.offsetTop &&
+        (!nextEl || scrollPos < nextEl.offsetTop)
+      ) {
+        currentLink.classList.add('current');
+      } else {
+        currentLink.classList.remove('current');
+      }
+    }
   });
 }
